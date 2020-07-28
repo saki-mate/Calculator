c86ab54d0d25095702c076fea99a920c2e661392
saki-mate <61312461+saki-mate@users.noreply.github.com>
2020-07-28 13:15:25 +0200

    Added division of x by y

--- Calc.cpp
+++ Calc.cpp
@@ -12,6 +12,7 @@ int main(){
   cout << "x + y = " << x+y << endl;
   cout << "x - y = " << x-y << endl;
   cout << "x * y = " << x*y << endl;
+  cout << "x / y = " << x/y << endl;
   
 return 0;
 }
