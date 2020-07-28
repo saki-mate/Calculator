c2d764dbb4cea2e137de1295ed4e2a3d20f268a2
saki-mate <61312461+saki-mate@users.noreply.github.com>
2020-07-28 13:26:40 +0200

    Added calculation of sin(x) and cos(x)

--- Calc.cpp
+++ Calc.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main(){
@@ -13,6 +15,10 @@ int main(){
   cout << "x - y = " << x-y << endl;
   cout << "x * y = " << x*y << endl;
   cout << "x / y = " << x/y << endl;
+
+  
+  cout << "sin(x) = " << sin(x * M_PI /180.0) << endl;
+  cout << "cos(x) = " << cos(x * M_PI /180.0) << endl;
   
 return 0;
 }
